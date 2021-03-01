4e83250df8a21aba867eb6af26640d162bada34a
Nelson Gonzalez <njeanpierre23@gmail.com>
2021-03-01 13:44:03 -0400

    Async, Await, Convert-Units y Skeleton

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import { Grid, List, ListItem } from '@material-ui/core';
+import convertUnits from 'convert-units';
 import CityInfo from '../CityInfo';
 import Weather from '../Weather';
+import { Alert } from '@material-ui/lab';
 
+
+const getCityCode = (city, countryCode) =>  `${city}-${countryCode}`
 // renderCitiesAndCountry se va a convertir en una funcion q retorna otra (HOC)
 const renderCitiesAndCountry = eventOnClickCity => {
     
     const renderCitiesAndCountryInternal = (cityAndCountry, weather) => {
-        const { city, country} = cityAndCountry;
+        const { city, country, countryCode} = cityAndCountry;
         //const { temperature, state} = weather;
         return (
-            <ListItem button key={city} onClick={eventOnClickCity}>
+            <ListItem button key={getCityCode(city, countryCode)} onClick={eventOnClickCity}>
                 <Grid container justify="center" alignItems="center">
                     <Grid item  xs={12} md={9} >
-                            <CityInfo city={city} country={country} />
+                        <CityInfo city={city} country={country} />
                     </Grid>
                     <Grid item xs={12} md={7}>
-                        {
-                            weather?
-                            <Weather temperature={weather.temperature} state={weather.state} />
-                            : "No hay datos"
-                        }
+                        <Weather temperature={weather && weather.temperature} state={weather && weather.state} />
                     </Grid>
                 </Grid> 
             </ListItem>
@@ -38,28 +38,95 @@ const CityList = ({cities, onClickCity }) => {
     // [Caracas-Venezuela] : { temperature, state}
     // [Bogotá-Colombia] : { temperature, state}
     // [City-Country] : { temperature, state}.... N
-    const [allWeather, setAllWeather] = useState({})
+    const [allWeather, setAllWeather] = useState({});
+    const [error, setError] = useState(null);
 
     useEffect( () => {
-        const setWeather = ( city , country, countryCode ) => {
-            const apiKey = 'e5eedc545565283d795914961ea9a7c0', url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`
-            axios.get(url).then( response =>  {
+        const setWeather = async ( city, countryCode ) => {
+            const apiKey = '30c3bc93dec3c873d219c20b98430f7f', url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`
+            
+            try {
+                
+                const response = await axios.get(url);
+    
                 const { data } = response
-               
-                const temperature = data.main.temp;
+                    
+                const temperature = Number(convertUnits(data.main.temp).from("K").to("C").toFixed(0));
+                
                 const state = data.weather[0].main.toLowerCase();
 
+                const propKeys = getCityCode( city, countryCode)
+                
+                const propValues = { temperature, state } 
+    
                 setAllWeather( (allWeather) => {
-
-                    const result  =  {...allWeather,  [`${city}-${country}`] : { temperature, state} };
-
+    
+                    const result  =  {...allWeather,  [propKeys] : propValues};
+    
                     return result;
-                }); 
-            })
+                });
+
+            } catch (error) {
+
+                if(error.response) // errores regresados por el server 
+                {
+                    const { data, status } = error.response;
+                    console.log(data, status);
+                    setError("Ha ocurrido un error en el servidor de clima");
+                    
+                }else if(error.request) // errores que no llegaron al server pero se hizo la peticion
+                {
+                    console.log("No se pudo acceder al server (Revisar Internet o No envio la request al server)");
+                    setError("Verifique la conexion con su servidor");
+                    
+                }else{
+                    
+                    console.log('error imprevisto');
+                    setError("Error al cargar datos");
+
+                }
+                
+            }
+            /*
+            // EJEMPLO CON PROMESA
+            axios.get(url)
+                .then( response =>  {
+                    const { data } = response
+                
+                    const temperature = Number(convertUnits(data.main.temp).from("K").to("C").toFixed(0));
+                    const state = data.weather[0].main.toLowerCase();
+
+                    setAllWeather( (allWeather) => {
+
+                        const result  =  {...allWeather,  [`${city}-${country}`] : { temperature, state} };
+
+                        return result;
+                    }); 
+                }).catch( error => {
+
+                    if(error.response) // errores regresados por el server 
+                    {
+                        const { data, status } = error.response;
+                        console.log(data, status);
+                        setError("Ha ocurrido un error en el servidor de clima");
+                        
+                    }else if(error.request) // errores que no llegaron al server pero se hizo la peticion
+                    {
+                        console.log("No se pudo acceder al server (Revisar Internet o No envio la request al server)");
+                        setError("Verifique la conexion con su servidor");
+                        
+                    }else{
+                        
+                        console.log('error imprevisto');
+                        setError("Error al cargar datos");
+                    
+                    }
+
+                });*/
            
         }
 
-        cities.forEach( ({ city, country, countryCode})=> setWeather( city, country, countryCode));
+        cities.forEach( ({ city, countryCode})=> setWeather( city, countryCode));
         
     }, [cities]);
 
@@ -67,9 +134,14 @@ const CityList = ({cities, onClickCity }) => {
 
     // const funcAux = renderCitiesAndCountry(onClickCity); // modo de simplificar un HOC
     return (
+        <div>
+            {
+                error && <Alert onClose={ () => setError(null) } severity="error">{error}</Alert>
+            }
         <List>
-            { cities.map( cityAndCountry => renderCitiesAndCountry(onClickCity)(cityAndCountry, allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]) ) }
+            { cities.map( cityAndCountry => renderCitiesAndCountry(onClickCity)(cityAndCountry, allWeather[getCityCode(cityAndCountry.city, cityAndCountry.countryCode)]) ) }
         </List>
+        </div>
     )
 }
 
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types';
 import { Grid, Typography } from '@material-ui/core';
 import { IconContext } from "react-icons";
 import IconState, {validValues} from '../IconState';
+import { Skeleton } from '@material-ui/lab';
+
 
 const Weather = ({ temperature, state}) => {
     return (
         <Grid container item direction="row" justify="center" alignItems="center" spacing={1}>
             <IconContext.Provider value={{ size: "5em" }}>
-                <IconState state={state} />
+                {
+                    state?
+                    <IconState state={state} />
+                    : <Skeleton variant="circle" heigth={80} width={80} />
+                }
             </IconContext.Provider>
+            {temperature? 
             <Typography display="inline" variant="h2">{ temperature }</Typography>
+            :<Skeleton  variant="rect" heigth={80} width={80} />}
         </Grid>
     
     )
